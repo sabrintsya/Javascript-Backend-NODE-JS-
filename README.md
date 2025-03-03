74bbf9e3ea7746d083bc37913395970279e0aafa
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2025-03-03 15:23:42 +0700

    add function http

---
+++ README.md
@@ -0,0 +1,7 @@
+Http : ada get ada post, memnculkan teks response
+required : memanggil javascript libary nya
+host name : domain awal url kita
+port ; port kita
+callback : memanggil kembali apa yang kita panggil
+request : data masuk dari luar
+resposnse : data keluar dari sistem
---
+++ script/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const host = 'localhost';
+const port = 3002;
+
+const server = http.createServer(function(request, response){
+    response.end('hello sabrina');
+});
+
+server.listen(port, host, function() {
+    console.log(`server running on ${host}:${port} 🐧 `);
+     
+});
